984b34517c95b77a6143445e53ae2cd2085ff8e1
Cassie C <cassie@colvindesign.com>
2021-08-23 21:04:50 -0400

    Added method to clamp out of range brightness and saturation values

--- src/main/java/color/GenColor.java
+++ src/main/java/color/GenColor.java
@@ -11,18 +11,56 @@ import java.util.Random;
  * Generates a color palette based off of a flat shade.
  */
 public class GenColor {
-    private static final int WIDTH = 20;
-    private static final int HEIGHT = 16;
+    private static final int WIDTH = 70;
+    private static final int HEIGHT = 70;
 
     public static void main(String[] args){
-        //Palette p = new Palette((int) (Math.random()*(360-1)+1),70,70,10,.06f);
-        Palette p = new Palette(32,96,90,10,.06f);
+        rainbowSpheres();
+        //int color = (int) (Math.random()*(359-1)+1);
+        int color = 26;
+        int sat = 79;
+        int bri = 38;
+        System.out.println("Color="+color+" Sat=50 Bri=50 Shift=15 Interval=.15f");
+        Palette p = new Palette(color,sat,bri,15,.15f);
         try {
             createBall(p);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static void rainbowSpheres(){
+        int color = (int) (Math.random()*(359-1)+1);
+        System.out.println("Color="+color+" Sat=50 Bri=50 Shift=15 Interval=.15f");
+        Palette p = new Palette(color,50,50,15,.15f);
+        //Palette p = new Palette(71,50,50,15,.15f);
+
+        BufferedImage b = new BufferedImage(1200,2200, BufferedImage.TYPE_4BYTE_ABGR);
+        try {
+            int x = 0;
+            int y = 0;
+            int counter = 0;
+            for(int i = 1; i<=360; i++){
+                p = new Palette(i,80,60,25,.15f);
+                createBall(p,b,x,y);
+
+                if(counter<=16){
+                    x+=70;
+                }else{
+                    x=0;
+                    y+=70;
+                    counter=0;
+                }
+                counter++;
+            }
+
+            File file = new File("generated_colors/spheres.png");
+
+            ImageIO.write(b, "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void createBall(Palette p) throws IOException {
@@ -46,19 +84,52 @@ public class GenColor {
 
         //Middle stripe, base color
         g2d.setColor(palette[2]);
-        g2d.fillOval(4,2,WIDTH-8,WIDTH-8);
+        g2d.fillOval(8,2,WIDTH-12,WIDTH-12);
 
         //Second to last stripe, should be darkest color
         g2d.setColor(palette[1]);
-        g2d.fillOval(6,1,WIDTH-12,WIDTH-12);
+        g2d.fillOval(12,3,WIDTH-20,WIDTH-20);
 
         //Last stripe, outline color
         g2d.setColor(palette[0]);
-        g2d.fillOval(7,1,WIDTH-16,WIDTH-16);
+        g2d.fillOval(26,6,WIDTH-45,WIDTH-45);
 
         File file = new File("generated_colors/test.png");
 
         ImageIO.write(b, "png", file);
+
+        g2d.dispose();
+    }
+
+    public static void createBall(Palette p, BufferedImage b, int startx, int starty) throws IOException {
+        int width = 70;
+        Graphics2D g2d = b.createGraphics();
+
+        Color[] palette = p.getPalette();
+
+
+        g2d.setColor(Color.white);
+        g2d.fillRect(startx,starty,width,width);
+
+        //First stripe on left, highlight
+        g2d.setColor(palette[4]);
+        g2d.fillOval(startx,starty,width,width);
+
+        //Second stripe on left, lightest color
+        g2d.setColor(palette[3]);
+        g2d.fillOval(startx+2,starty+2,width-4,width-4);
+
+        //Middle stripe, base color
+        g2d.setColor(palette[2]);
+        g2d.fillOval(startx+8,starty+2,width-12,width-12);
+
+        //Second to last stripe, should be darkest color
+        g2d.setColor(palette[1]);
+        g2d.fillOval(startx+12,starty+3,width-20,width-20);
+
+        //Last stripe, outline color
+        g2d.setColor(palette[0]);
+        g2d.fillOval(startx+26,starty+6,width-45,width-45);
 
         g2d.dispose();
     }
--- src/main/java/color/Palette.java
+++ src/main/java/color/Palette.java
@@ -39,18 +39,18 @@ public class Palette {
         if(color <= 30 || color > 330){
             //Do not hue shift
             if(color <=10 || color >= 350){
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);    //outline
             }
             else{
-                colors[0] = Color.getHSBColor((color+shift*2)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color+shift)/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color-shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color-shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color+shift*2,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color+shift,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color-shift,sat,bri-interval);       //darker
+                colors[4] = shiftColor(color-shift*2,sat-sat/4,bri-interval*2);    //outline
             }
         }
 
@@ -62,17 +62,17 @@ public class Palette {
         if(color > 30 && color <= 90){
             // Darken towards green if leaning towards green, otherwise towards red
             if(color>60){
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[0] = Color.getHSBColor((color+shift)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color+shift*2)/360f,sat-interval,bri+interval);        //lighter
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color+shift,sat,bri-interval);   //highlight
+                colors[4] = shiftColor(color+shift*2,sat-sat/4,bri-interval*2);        //lighter
             }else{
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color-shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color-shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);        //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color-shift,sat,bri-interval);      //darker
+                colors[4] = shiftColor(color-shift*2,sat-sat/4,bri-interval*2);    //outline
             }
         }
 
@@ -85,25 +85,25 @@ public class Palette {
         if(color>90 && color <= 150){
             //Do not hue shift
             if(color >=110 && color <= 130){
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);    //outline
             }
             else if(color < 110){       //Only yellow shift
-                colors[0] = Color.getHSBColor((color-shift*2)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color-shift)/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color-shift*2,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color-shift,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color,sat,bri-interval);      //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);    //outline
             }
             else{                       //Only cyan shift shift
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[0] = Color.getHSBColor((color+shift)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color+shift*2)/360f,sat-interval,bri+interval);        //lighter
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);        //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color+shift,sat,bri-interval);   //highlight
+                colors[4] = shiftColor(color+shift*2,sat-sat/4,bri-interval*2);        //lighter
             }
         }
 
@@ -115,17 +115,17 @@ public class Palette {
         if(color>150 && color <= 210){
             // Darken towards green, else darken towards blue
             if(color<=180){
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color-shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color-shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color-shift,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color-shift*2,sat-sat/4,bri-interval*2);    //outline
             }else{
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color+shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color+shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);     //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color+shift,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color+shift*2,sat-sat/4,bri-interval*2);    //outline
             }
         }
 
@@ -136,17 +136,17 @@ public class Palette {
          */
         if(color>210 && color <= 270){
             if(color>240){  //Lighten towards cyan, no dark shift
-                colors[0] = Color.getHSBColor((color-shift*2)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color-shift)/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color-shift*2,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color-shift,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);   //outline
             }else{          //Darken towards purple, no light shift
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color+shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color+shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);    //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color+shift,sat,bri-interval);     //darker
+                colors[4] = shiftColor(color+shift*2,sat-sat/4,bri-interval*2);    //outline
             }
         }
 
@@ -158,25 +158,25 @@ public class Palette {
         if(color>270 && color <=330){
             //Do not hue shift
             if(color >=290 && color <= 310){
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);   //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color,sat,bri-interval);        //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);    //outline
             }
             else if(color < 290){       //Dark shift towards blue
-                colors[0] = Color.getHSBColor(color/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor(color/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor((color-shift)/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor((color-shift*2)/360f,sat+interval*4,bri-interval*4);    //outline
+                colors[0] = shiftColor(color,sat-sat/2,bri+interval*2);  //highlight
+                colors[1] = shiftColor(color,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                                 //base
+                colors[3] = shiftColor(color-shift,sat,bri-interval);      //darker
+                colors[4] = shiftColor(color-shift*2,sat-sat/4,bri-interval*2);   //outline
             }
-            else{                       //Light shift to red
-                colors[0] = Color.getHSBColor((color+shift*2)/360f,sat-interval*2,bri+interval*2);    //highlight
-                colors[1] = Color.getHSBColor((color+shift)/360f,sat-interval,bri+interval);        //lighter
-                colors[2] = Color.getHSBColor(color/360f,sat,bri);                                  //base
-                colors[3] = Color.getHSBColor(color/360f,sat+interval,bri-interval);        //darker
-                colors[4] = Color.getHSBColor(color/360f,sat+interval*4,bri-interval*4);    //outline
+            else{//Light shift to red
+                colors[0] = shiftColor(color+shift*2,sat-sat/2,bri+interval*2);  //highlight
+                colors[1] = shiftColor(color+shift,sat,bri+interval);       //lighter
+                colors[2] = shiftColor(color,sat,bri);                               //base
+                colors[3] = shiftColor(color,sat,bri-interval);     //darker
+                colors[4] = shiftColor(color,sat-sat/4,bri-interval*2);  //outline
             }
         }
 
@@ -195,5 +195,35 @@ public class Palette {
         }*/
 
         return colors;
+    }
+
+    private static Color shiftColor(int hue, float sat, float bri){
+        float brightness = bri;
+        float changeSat = 0;
+
+        System.out.println("brightness: "+bri);
+
+        if(bri<0){
+            brightness = 0f;
+            changeSat=-bri-.1f;
+        }
+        else if (bri>1){
+            brightness = 1f;
+            changeSat=1f-bri+.1f;
+        }
+        else
+            brightness = bri;
+
+        float newSat = sat+changeSat;
+        System.out.println("newSat: "+newSat);
+
+        if(newSat<0f)
+            newSat = 0f;
+        else if (newSat>1f)
+            newSat = 1f;
+
+
+        Color c = Color.getHSBColor(hue/360f,newSat,brightness);
+        return c;
     }
 }
